af9539b0233077b7d91cb9c6436da594fb7c24f9
illah <ilahdilah3@gmail.com>
2015-06-26 10:06:03 +0700

    dillah4

---
+++ Laporan4/src/conditional.java
@@ -0,0 +1,17 @@
+
+public class conditional {
+	public static void main (String[] args) {
+		int nilai1 = 1;
+		int nilai2 = 2;
+		int hasil;
+		boolean kondisi = true;
+		hasil = kondisi ? nilai1 : nilai2;
+		if (kondisi == true)
+			hasil = nilai1;
+		else
+			hasil = nilai2;
+		
+		System.out.println (hasil);
+	}
+
+}
